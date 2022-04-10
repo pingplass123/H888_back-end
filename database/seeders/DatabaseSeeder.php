64de8b2dcc3adaead722c25bc9cac3a86c637f4c
pingplass123 <60729252+pingplass123@users.noreply.github.com>
2022-04-10 22:56:04 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +16,43 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('users')->insert([
+            'id' => 1,
+            'name' => 'pingplass1',
+            'password' => Hash::make('670qeycc'),
+        ]);
+
+
+        DB::table('users')->insert([
+            'id' => 2,
+            'name' => 'pingplass2',
+            'password' => Hash::make('670qeycc'),
+        ]);
+
+
+        DB::table('users')->insert([
+            'id' => 3,
+            'name' => 'pingplass3',
+            'password' => Hash::make('670qeycc'),
+        ]);
+
+        DB::table('owner')->insert([
+            'idOwner' => 1,
+            'idUser' => 1,
+        ]);
+
+        DB::table('admins')->insert([
+            'idAdmin' => 1,
+            'idUser' => 2,
+            'name' => 'ping',
+        ]);
+
+        DB::table('customers')->insert([
+            'idCustomer' => 1,
+            'idUser' => 3,
+            'name' => 'pingCustomers',
+            'created_by' => 2,
+        ]);
+
     }
 }
