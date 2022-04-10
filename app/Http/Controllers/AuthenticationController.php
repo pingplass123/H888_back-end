21f384bf12e164666724f7d1d917f301c2e846b9
Charinpat Pukhajeetrakul <charinpat.p@gmail.com>
2022-04-10 21:35:10 +0700

    Login API

---
+++ app/Http/Controllers/AuthenticationController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthenticationController extends Controller
+{
+    public function login(Request $request)
+    {
+        if(auth()->attempt(array('username' => $request->username, 'password' => $request->password)))
+        {
+            $user = Auth::user(); 
+            $role = "";
+
+            if($user->isOwner() != [])
+            {
+                $role = "owner";
+            }
+            elseif($user->isAdmin() != [])
+            {
+                $role = "admin";
+            }
+            elseif($user->isCustomer() != [])
+            {
+                $role = "customer";
+            }
+
+            $success['token'] =  $user->createToken('H888')->plainTextToken; 
+            $success['name'] =  $user->username;
+            $success['role'] =  $role;
+
+            return $this->sendResponse($success, 'User login successfully.');
+        } 
+        else{ 
+            return $this->sendError('Unauthorised.', ['error'=>'Unauthorised']);
+        } 
+    }
+}
